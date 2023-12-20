61bcbc5912f7a71c985647ac24eb5bcdd5297c5b
Pål <pkn@ssb.no>
2023-12-20 15:34:23 +0100

    Forenklet beregning av barn i barnehager.

--- laerermod.py
+++ laerermod.py
@@ -21,7 +21,7 @@ dem4 = 'inndata/antall_studenter_hoyereutdanning.txt'
 
 innpr = 'inndata/endring_standard.txt'
 inplu = 'inndata/endring_timeverk.txt'
-vak = 'inndata/vakanse.txt'
+vakanse = 'inndata/vakanse.txt'
 
 
 starttid = time.time()
@@ -143,7 +143,8 @@ LærerStudenterTotalt = pd.DataFrame(LærerStudenter.groupby(["Utdanning"]).sum(
 LærerStudenterTotalt.rename(columns={"Alle": "totalt"}, inplace=True)
 LærerStudenterTotalt.drop(['Alder', 'Kvinner', 'Menn'], axis=1, inplace=True)
 
-LærerStudenter = LærerStudenter.merge(LærerStudenterTotalt, how='outer', on='Utdanning')
+LærerStudenter = LærerStudenter.merge(LærerStudenterTotalt, how='inner', on='Utdanning')
+
 LærerStudenter.Menn = LærerStudenter.Menn / LærerStudenter.totalt
 LærerStudenter.Kvinner = LærerStudenter.Kvinner / LærerStudenter.totalt
 LærerStudenter = LærerStudenter.reset_index()
@@ -154,25 +155,14 @@ LærerStudenter = LærerStudenter.reset_index()
 
 barnhin = pd.DataFrame(pd.read_csv(dem1,
                       header=None,
-                      delimiter=" ",
-                      names=['År', 'TimerMin', 'TinerMax', 'Alder0', 'Alder1', 'Alder2', 'Alder3', 'Alder4', 'Alder5'],
-                      usecols=list(range(9))))
+                      delimiter="\s+",
+                      names=['TimerMin', 'TinerMax', 'Alder0', 'Alder1', 'Alder2', 'Alder3', 'Alder4', 'Alder5'],
+                      usecols=list(range(8))))
 
-barn1 = pd.DataFrame()
-barn1["b2021"] = barnhin.Alder0
-barn1["tim"] = barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)
-
-barn2 = pd.DataFrame()
-barn2["b2021"] = barnhin.Alder1 + barnhin.Alder2
-barn2["tim"] = barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)
-
-barn3 = pd.DataFrame()
-barn3["b2021"] = barnhin.Alder3
-barn3["tim"] = barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)
-
-barn4 = pd.DataFrame()
-barn4["b2021"] = barnhin.Alder4 + barnhin.Alder5
-barn4["tim"] = barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)
+barn1 = pd.DataFrame({'b2021': barnhin.Alder0, 'tim': barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)})
+barn2 = pd.DataFrame({'b2021': barnhin.Alder1 + barnhin.Alder2, 'tim': barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)})
+barn3 = pd.DataFrame({'b2021': barnhin.Alder3, 'tim': barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)})
+barn4 = pd.DataFrame({'b2021': barnhin.Alder4 + barnhin.Alder5, 'tim': barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)})
 
 # ************************************
 # Oppretter noen oppsummeringstabeller
@@ -181,65 +171,49 @@ barn4["tim"] = barnhin.TimerMin + ((barnhin.TinerMax - barnhin.TimerMin) / 2)
 befs1 = pd.DataFrame({'agr2020': bef.query('Alder == 0').a2020.sum(),
                       'agr2021': bef.query('Alder == 0').a2021.sum(),
                       'ald1': 0,
-                      'ald2': 0}, index=[0])
+                      'ald2': 0,
+                      'Populasjon': barn1.b2021.sum(),
+                      'Brukerindeks': (2 * barn1.b2021.mul(barn1.tim.values).sum()) /
+                                      (barn1.b2021.sum() * 42.5)}, index=[0])
 
 befs2 = pd.DataFrame({'agr2020': bef.query('Alder >= 1 and Alder <= 2').a2020.sum(),
                       'agr2021': bef.query('Alder >= 1 and Alder <= 2').a2021.sum(),
                       'ald1': 1,
-                      'ald2': 2}, index=[0])
+                      'ald2': 2,
+                      'Populasjon': barn2.b2021.sum(),
+                      'Brukerindeks': (2 * barn2.b2021.mul(barn2.tim.values).sum()) /
+                                      (barn2.b2021.sum() * 42.5)}, index=[0])
 
 befs3 = pd.DataFrame({'agr2020': bef.query('Alder == 3').a2020.sum(),
                       'agr2021': bef.query('Alder == 3').a2021.sum(),
                       'ald1': 3,
-                      'ald2': 3}, index=[0])
+                      'ald2': 3,
+                      'Populasjon': barn3.b2021.sum(),
+                      'Brukerindeks': (1.5 * barn3.b2021.mul(barn3.tim.values).sum()) /
+                                      (barn3.b2021.sum() * 42.5)}, index=[0])
 
 befs4 = pd.DataFrame({'agr2020': bef.query('Alder >= 4 and Alder <= 5').a2020.sum(),
                       'agr2021': bef.query('Alder >= 4 and Alder <= 5').a2021.sum(),
                       'ald1': 4,
-                      'ald2': 5}, index=[0])
-
-barna1 = pd.DataFrame({'ald1': 0,
-                       'ald2': 0,
-                       'br': barn1.b2021.sum(),
-                       'bri': (2 * barn1.b2021.mul(barn1.tim.values).sum()) /
-                              (barn1.b2021.sum() * 42.5)}, index=[0])
-
-barna2 = pd.DataFrame({'ald1': 1,
-                       'ald2': 2,
-                       'br': barn2.b2021.sum(),
-                       'bri': (2 * barn2.b2021.mul(barn2.tim.values).sum()) /
-                              (barn2.b2021.sum() * 42.5)}, index=[0])
-
-barna3 = pd.DataFrame({'ald1': 3,
-                       'ald2': 3,
-                       'br': barn3.b2021.sum(),
-                       'bri': (1.5 * barn3.b2021.mul(barn3.tim.values).sum()) /
-                              (barn3.b2021.sum() * 42.5)}, index=[0])
-
-barna4 = pd.DataFrame({'ald1': 4,
-                       'ald2': 5,
-                       'br': barn4.b2021.sum(),
-                       'bri': (1 * barn4.b2021.mul(barn4.tim.values).sum()) /
-                              (barn4.b2021.sum() * 42.5)}, index=[0])
+                      'ald2': 5,
+                      'Populasjon': barn4.b2021.sum(),
+                      'Brukerindeks': (1 * barn4.b2021.mul(barn4.tim.values).sum()) /
+                                      (barn4.b2021.sum() * 42.5)}, index=[0])
 
 # ********************
 # Slår sammen tabeller
 # ********************
 
-barnr = pd.concat([befs1, befs2, befs3, befs4], ignore_index=True)
-barnar = pd.concat([barna1, barna2, barna3, barna4], ignore_index=True)
+demo1 = pd.concat([befs1, befs2, befs3, befs4], ignore_index=True)
 
-barnr["br"] = barnar.br
-barnr["bri"] = barnar.bri
+demo1["ans1"] = demo1.Populasjon / demo1.agr2021
+demo1["ans2"] = 1.12 * demo1.ans1
 
-barnr["ans1"] = barnr.br / barnr.agr2021
-barnr["ans2"] = 1.12 * barnr.ans1
-
-barnr['ans2'] = barnr.apply(lambda row: 1.05 * row['ans1']
+demo1['ans2'] = demo1.apply(lambda row: 1.05 * row['ans1']
                             if row['ald1'] == 0
                             else row['ans2'], axis=1)
 
-barnr['ans2'] = barnr.apply(lambda row: 0.97
+demo1['ans2'] = demo1.apply(lambda row: 0.97
                             if row['ans2'] > 0.95
                             else row['ans2'], axis=1)
 
@@ -250,7 +224,7 @@ barnr['ans2'] = barnr.apply(lambda row: 0.97
 Gjennomføring = pd.DataFrame(pd.read_fwf(stkap,
                      header=None,
                      delimiter=" ",
-                     names=["Utdanning", "NormertTid", "fullfor", "fullfob"]))
+                     names=["Utdanning", "NormertTid", "FullføringNye", "FullføringIgangværende"]))
 
 OpptatteLærerStudenter = pd.DataFrame(pd.read_fwf(oppta,
                      header=None,
@@ -260,7 +234,7 @@ OpptatteLærerStudenter = pd.DataFrame(pd.read_fwf(oppta,
 plussakt = pd.DataFrame(pd.read_fwf(inplu,
                        header=None,
                        delimiter=" ",
-                       names=["Alder", "plussm", "plussk"]))
+                       names=["Alder", "EndringMenn", "Endringkvinner"]))
 
 nystud1 = LærerStudenter.copy()
 nystud1['Kjønn'] = 1
@@ -279,7 +253,6 @@ PopulasjonUtdannedeLærere["Årsverk"] = PopulasjonUtdannedeLærere.Populasjon *
 PopulasjonUtdannedeLærere.drop(['Sysselsatte', 'Sysselsettingsandel', 'GjennomsnitteligeÅrsverk'], axis=1, inplace=True)
 
 beh_syss = UtdannedeLærere.copy()
-
 beh_syss.drop(['Populasjon', 'Sysselsatte'], axis=1, inplace=True)
 
 EtterspørselLærere = pd.DataFrame({'Utdanning': ['ba', 'gr', 'fa', 'ph', 'py']})
@@ -287,10 +260,10 @@ EtterspørselLærere = pd.DataFrame({'Utdanning': ['ba', 'gr', 'fa', 'ph', 'py']
 for i in range(1, 7):
     EtterspørselLærere["År"+str(i)] = SysselsatteLærere.ÅrsverkTotalt[SysselsatteLærere.ÅrsverkTotalt.index.get_level_values('Sektor') == i].reset_index(drop=True)
 
-vakesp = pd.DataFrame(pd.read_fwf(vak,
+VakanseEtterspørsel = pd.DataFrame(pd.read_fwf(vakanse,
                      header=None,
                      delimiter=" ",
-                     names=["Utdanning", "vak1", "vak2", "vak3", "vak4", "vak5", "vak6"]))
+                     names=["Utdanning", "VakanseSektor1", "VakanseSektor2", "VakanseSektor3", "VakanseSektor4", "VakanseSektor5", "VakanseSektor6"]))
 
 # ********************************************************
 # PROSENTVIS ENDRING I ANTALL ELEVER pr. 1000 INNBYGGERE
@@ -298,98 +271,48 @@ vakesp = pd.DataFrame(pd.read_fwf(vak,
 # tallet 1.01 tolkes som 1 prosent økt elevtall pr. 1000
 # ********************************************************
 
-stand = pd.DataFrame(pd.read_fwf(innpr,
+Standardendring = pd.DataFrame(pd.read_fwf(innpr,
                     header=None,
                     delimiter=" ",
                     names=["År", "Sektor1", "Sektor2", "Sektor3", "Sektor4", "Sektor5", "Sektor6"]))
-
-stand = stand[stand['År'] >= Basisår]
-stand = stand[stand['År'] <= Sluttår]
 
 # ****************************************
 # Oppretter datasett for senere utfylling.
 # ****************************************
 
-ald1 = pd.DataFrame(columns=['ald2', 'Alder'])
+ald1 = pd.DataFrame({"ald2": [0, 2, 2, 3, 5, 5],
+                     "Alder": [0, 1, 2, 3, 4, 5]})
 
-for x in range(0, 1):
-    nyrad = {'ald2': 0, 'Alder': x}
-    ald1.loc[len(ald1)] = nyrad
-
-for x in range(1, 3):
-    nyrad = {'ald2': 2, 'Alder': x}
-    ald1.loc[len(ald1)] = nyrad
-
-for x in range(3, 4):
-    nyrad = {'ald2': 3, 'Alder': x}
-    ald1.loc[len(ald1)] = nyrad
-
-for x in range(4, 6):
-    nyrad = {'ald2': 5, 'Alder': x}
-    ald1.loc[len(ald1)] = nyrad
-
-ald2 = pd.DataFrame(columns=['ald2', 'Alder'])
-
-for x in range(6, 16):
-    nyrad = {'ald2': 15, 'Alder': x}
-    ald2.loc[len(ald2)] = nyrad
+ald2 = pd.DataFrame({"ald2": [15, 15, 15, 15, 15, 15, 15, 15, 15, 15],
+                     "Alder": [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]})
 
 ald3 = pd.DataFrame(columns=['ald2', 'Alder'])
 
 for x in range(0, 16):
-    nyrad = {'ald2': 15, 'Alder': x}
-    ald3.loc[len(ald3)] = nyrad
+    ald3.loc[len(ald3)] = {'ald2': 15, 'Alder': x}
 
 for x in range(16, 25):
-    nyrad = {'ald2': x, 'Alder': x}
-    ald3.loc[len(ald3)] = nyrad
+    ald3.loc[len(ald3)] = {'ald2': x, 'Alder': x}
 
 for x in range(25, 100):
-    nyrad = {'ald2': 99, 'Alder': x}
-    ald3.loc[len(ald3)] = nyrad
+    ald3.loc[len(ald3)] = {'ald2': 99, 'Alder': x}
 
-ald4 = pd.DataFrame(columns=['ald2', 'Alder'])
+ald4 = pd.DataFrame({"ald2": [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 34, 34, 34, 34, 34, 39, 39, 39, 39, 39, 44, 44, 44, 44, 44, 49, 49, 49, 49, 49],
+                     "Alder": [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]})
 
-for x in range(19, 30):
-    nyrad = {'ald2': x, 'Alder': x}
-    ald4.loc[len(ald4)] = nyrad
-
-for x in range(30, 35):
-    nyrad = {'ald2': 34, 'Alder': x}
-    ald4.loc[len(ald4)] = nyrad
-
-for x in range(35, 40):
-    nyrad = {'ald2': 39, 'Alder': x}
-    ald4.loc[len(ald4)] = nyrad
-
-for x in range(40, 45):
-    nyrad = {'ald2': 44, 'Alder': x}
-    ald4.loc[len(ald4)] = nyrad
-
-for x in range(45, 50):
-    nyrad = {'ald2': 49, 'Alder': x}
-    ald4.loc[len(ald4)] = nyrad
-print(ald4)
 ald5 = pd.DataFrame(columns=['ald2', 'Alder'])
-
-for x in range(0, 100):
-    nyrad = {'ald2': 99, 'Alder': x}
-    ald5.loc[len(ald5)] = nyrad
-
 ald6 = pd.DataFrame(columns=['ald2', 'Alder'])
 
 for x in range(0, 100):
-    nyrad = {'ald2': 99, 'Alder': x}
-    ald6.loc[len(ald6)] = nyrad
-
-demo1 = barnr
+    ald5.loc[len(ald5)] = {'ald2': 99, 'Alder': x}
+    ald6.loc[len(ald6)] = {'ald2': 99, 'Alder': x}    
 
 demo2 = pd.DataFrame({'ald1': 6,
                       'ald2': 15,
-                      'br': bef.query('Alder >= 6 and Alder <= 15').a2021.sum(),
-                      'bri': 1.0}, index=[0])
+                      'Populasjon': bef.query('Alder >= 6 and Alder <= 15').a2021.sum(),
+                      'Brukerindeks': 1.0}, index=[0])
 
-kolonnenavn = ['ald1', 'ald2', 'br', 'bri']
+kolonnenavn = ['ald1', 'ald2', 'Populasjon', 'Brukerindeks']
 
 demo3 = pd.DataFrame(pd.read_fwf(dem3,
                     header=None,
@@ -403,13 +326,13 @@ demo4 = pd.DataFrame(pd.read_fwf(dem4,
 
 demo5 = pd.DataFrame({'ald1': 0,
                       'ald2': 99,
-                      'br': bef.a2021.sum(),
-                      'bri': 1.0}, index=[0])
+                      'Populasjon': bef.a2021.sum(),
+                      'Brukerindeks': 1.0}, index=[0])
 
 demo6 = pd.DataFrame({'ald1': 0,
                       'ald2': 99,
-                      'br': bef.a2021.sum(),
-                      'bri': 1.0}, index=[0])
+                      'Populasjon': bef.a2021.sum(),
+                      'Brukerindeks': 1.0}, index=[0])
 
 # ************************************************
 # LAGER ALDERSAGGREGATER av befolkningsfilen etter
@@ -417,8 +340,6 @@ demo6 = pd.DataFrame({'ald1': 0,
 # ************************************************
 
 for i in range(1, 7):
-    locals()[f'bef{i}'] = pd.DataFrame()
-
     locals()[f'bef{i}'] = locals()[f'ald{i}'].merge(bef, how='inner', on='Alder')
 
     locals()[f'bef{i}'] = locals()[f'bef{i}'].groupby(["ald2"]).sum()
@@ -431,13 +352,13 @@ for i in range(1, 7):
     for x in range(Basisår, Sluttår + 1):
         locals()[f'demo{i}']['agr' + str(x)] = locals()[f'bef{i}']['a' + str(x)]
 
-    locals()[f'demo{i}']["pg" + str(Basisår)] = locals()[f'demo{i}'].br * locals()[f'demo{i}'].bri
+    locals()[f'demo{i}']["pg" + str(Basisår)] = locals()[f'demo{i}'].Populasjon * locals()[f'demo{i}'].Brukerindeks
 
     for x in range(Basisår + 1, Sluttår + 1):
         locals()[f'demo{i}']['pg' + str(x)] = locals()[f'demo{i}']['pg' + str(x-1)] * (locals()[f'bef{i}']['a' + str(x)] /
                                                          locals()[f'bef{i}']['a' + str(x-1)])
 
-    locals()[f'demo{i}']["mg" + str(Basisår)] = locals()[f'demo{i}'].br * locals()[f'demo{i}'].bri
+    locals()[f'demo{i}']["mg" + str(Basisår)] = locals()[f'demo{i}'].Populasjon * locals()[f'demo{i}'].Brukerindeks
 
     for x in range(Basisår + 1, Sluttår + 1):
         locals()[f'demo{i}']['mg' + str(x)] = locals()[f'demo{i}']['mg' + str(x-1)] * (locals()[f'bef{i}']['a' + str(x)] /
@@ -455,7 +376,6 @@ for i in range(1, 7):
     locals()[f'demy{i}'] = pd.DataFrame({'Utdanning': ['ba', 'gr', 'fa', 'ph', 'py'],
                                          'Brukerindeks': [y, y, y, y, y]})
     
-    locals()[f'Årsverk{i}'] = pd.DataFrame()
     locals()[f'Årsverk{i}'] = locals()[f'demy{i}']
     
     locals()[f'Årsverk{i}']['År'] = EtterspørselLærere['År' + str(i)]
@@ -476,7 +396,7 @@ for i in range(1, 7):
 # etter tjenester
 # ******************************************************
 
-dmindeks = stand.merge(demår1, how='inner', on='År')
+dmindeks = Standardendring.merge(demår1, how='inner', on='År')
 dmindeks = dmindeks.merge(demår2, how='inner', on='År')
 dmindeks = dmindeks.merge(demår3, how='inner', on='År')
 dmindeks = dmindeks.merge(demår4, how='inner', on='År')
@@ -492,14 +412,14 @@ dmindeks = dmindeks.merge(demår6, how='inner', on='År')
 
 LærerKandidaterTotalt = OpptatteLærerStudenter.merge(Gjennomføring, how='inner', on='Utdanning')
 
-LærerKandidaterTotalt['uteks'] = LærerKandidaterTotalt.apply(lambda row: (row['OpptatteStudenter'] * row['fullfob'])
+LærerKandidaterTotalt['Uteksaminerte'] = LærerKandidaterTotalt.apply(lambda row: (row['OpptatteStudenter'] * row['FullføringIgangværende'])
                                  if row['År'] + row['NormertTid'] <= Basisår + 3
-                                 else (row['OpptatteStudenter'] * row['fullfor']), axis=1)
+                                 else (row['OpptatteStudenter'] * row['FullføringNye']), axis=1)
 
 kand_ald = nystud.merge(LærerKandidaterTotalt, how='inner', on=['Utdanning'])
 
 kand_ald["Alder"] = kand_ald.Alder + kand_ald.NormertTid
-kand_ald["eks_ald"] = kand_ald.uteks * kand_ald.st_ald
+kand_ald["eks_ald"] = kand_ald.Uteksaminerte * kand_ald.st_ald
 
 # *************************************************************
 # NYBEHOLD: Fører strømmen av nyutdannete inn i beholdningen av
@@ -576,17 +496,17 @@ ind5 = dmindeks.copy()
 ind = pd.concat([ind1, ind2, ind3, ind4, ind5])
 ind = ind.reset_index()
 
-Etterspørsel = reduce(lambda left, right: pd.merge(left, right, on=['Utdanning'], how='outer'), [ind, EtterspørselLærere, vakesp])
+Etterspørsel = reduce(lambda left, right: pd.merge(left, right, on=['Utdanning'], how='outer'), [ind, EtterspørselLærere, VakanseEtterspørsel])
 
 Etterspørsel = Etterspørsel.reset_index()
 Etterspørsel = Etterspørsel.set_index(['Utdanning', 'År'])
 
-Etterspørsel['Etterspørsel'] = (Etterspørsel.År1 + Etterspørsel.vak1) * Etterspørsel.dm1 * Etterspørsel.Sektor1
-Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År2 + Etterspørsel.vak2) * Etterspørsel.dm2 * Etterspørsel.Sektor2
-Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År3 + Etterspørsel.vak3) * Etterspørsel.dm3 * Etterspørsel.Sektor3
-Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År4 + Etterspørsel.vak4) * Etterspørsel.dm4 * Etterspørsel.Sektor4
-Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År5 + Etterspørsel.vak5) * Etterspørsel.dm5 * Etterspørsel.Sektor5
-Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År6 + Etterspørsel.vak6) * Etterspørsel.dm6 * Etterspørsel.Sektor6
+Etterspørsel['Etterspørsel'] = (Etterspørsel.År1 + Etterspørsel.VakanseSektor1) * Etterspørsel.dm1 * Etterspørsel.Sektor1
+Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År2 + Etterspørsel.VakanseSektor2) * Etterspørsel.dm2 * Etterspørsel.Sektor2
+Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År3 + Etterspørsel.VakanseSektor3) * Etterspørsel.dm3 * Etterspørsel.Sektor3
+Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År4 + Etterspørsel.VakanseSektor4) * Etterspørsel.dm4 * Etterspørsel.Sektor4
+Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År5 + Etterspørsel.VakanseSektor5) * Etterspørsel.dm5 * Etterspørsel.Sektor5
+Etterspørsel['Etterspørsel'] = Etterspørsel['Etterspørsel'] + (Etterspørsel.År6 + Etterspørsel.VakanseSektor6) * Etterspørsel.dm6 * Etterspørsel.Sektor6
 
 TilbudOgEtterspørsel = Tilbud.merge(Etterspørsel, how='outer', on=['Utdanning', 'År'])
 
